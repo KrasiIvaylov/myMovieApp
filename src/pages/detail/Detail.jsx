e9b43dbcd77b3767cb778cb2470403a6788ea0a4
KrasiIvaylov <krasimirivajlov@gmail.com>
2022-05-01 18:32:09 +0300

    adding id to localstorage

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -17,24 +17,25 @@ const Detail = () => {
   const [item, setItem] = useState(null);
   const [favourites, setFavourites] = useState([]);
 
+  useEffect(() => {
+    const movieFavourites = JSON.parse(localStorage.getItem("favourites"));
+
+    if (movieFavourites) {
+      setFavourites(movieFavourites);
+    }
+  }, []);
+
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem("favourites", JSON.stringify(items));
+  };
+
   const AddFavouriteMovie = (movie) => {
-    // useEffect(() => {
-    //   const movieFavourites = JSON.parse(localStorage.getItem("favourites"));
-
-    //   if (movieFavourites) {
-    //     setFavourites(movieFavourites);
-    //   }
-    // }, []);
-
     movie = item.id;
-
-    const saveToLocalStorage = (items) => {
-      localStorage.setItem("favourites", items);
-    };
 
     const newFavouriteList = [...favourites, movie];
     setFavourites(newFavouriteList);
     saveToLocalStorage(newFavouriteList);
+
     console.log(movie);
   };
 
